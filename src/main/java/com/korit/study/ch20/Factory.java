65ce8940c8ad70f99fa9960e7b7a20a0266ddce1
jiny-01 <allie7019@naver.com>
2025-10-16 15:35:31 +0900

    ìch20: Factory 실습, static 사용í해보기

--- src/main/java/com/korit/study/ch20/Factory.java
+++ src/main/java/com/korit/study/ch20/Factory.java
@@ -1,10 +1,16 @@
 package com.korit.study.ch20;
 
 public class Factory {
+    private static int id;
     private String name;
 
     public Factory(String name) {
         this.name = name;
+    }
+
+    public static Product createProduct(String name) {
+        return new Product(++id, name);
+        //공장끼리 이 static id를 공유하기 때문에 공장이 1, 2, 3 생성될 떄마다 id 를 공유함
     }
 
     @Override
