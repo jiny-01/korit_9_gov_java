68d1667a730ff16fe67211b4ffd12319d5812956
jiny-01 <allie7019@naver.com>
2025-10-16 17:07:49 +0900

    ìch21 : 싱글톤

--- src/main/java/com/korit/study/ch21/Singleton.java
+++ src/main/java/com/korit/study/ch21/Singleton.java
@@ -3,12 +3,13 @@ package com.korit.study.ch21;
 import java.util.Objects;
 
 public class Singleton {
-    private static Singleton instance = new Singleton("Class Data");
+    private volatile static Singleton instance = new Singleton("Class Data");
     // static 으로 만들어진 singleton 객체를 서로 다른 여러 곳에서 공유해서 사용
     //*싱글톤 => 오직 하나의 인스턴스만 가짐, 무조건 하나의 객체를 공유해서 사용할 때 (외부에서 다시 생성되면 안됨)
     //그래서 싱글톤 생성자의 생성 자체를 막기 위해 생성자의 접근 지정자를 private 로
     // 싱글톤 객체의 변수명은 항상 instance 로 통일
     //외부에서 호출하기 위해 getter 사용
+    //volatile : 휘발되지 않는 메모리
 
     private String data;
 
@@ -32,6 +33,7 @@ public class Singleton {
                 if (Objects.isNull(instance)) {    //진짜 작업 중 아닌 게 확인되면 그때서야 생성하는 것
                     instance = new Singleton("Class Data");
                 }
+                return instance;
             }
         }
     }
