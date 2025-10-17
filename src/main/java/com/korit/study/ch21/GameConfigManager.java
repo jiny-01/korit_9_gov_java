036e9657281f795c35e8226d9c624921a4cfff74
jiny-01 <allie7019@naver.com>
2025-10-17 10:37:59 +0900

    싱ê¸ch21 : 싱글톤 실습

--- src/main/java/com/korit/study/ch21/GameConfigManager.java
+++ src/main/java/com/korit/study/ch21/GameConfigManager.java
@@ -4,27 +4,43 @@ import java.util.Objects;
 
 public class GameConfigManager {
     // TODO: 싱글톤 구현을 위한 필드들
-    private static GameConfigManager instance = new GameConfigManager(0, "");
+    private static GameConfigManager instance;
 
     // 설정 정보 필드들
     private int soundVolume = 50;           // 기본값 50
     private String resolution = "1280x720"; // 기본 해상도
 
     // TODO: 생성자 구현
-    private GameConfigManager(int soundVolume, String resolution) {
-        this.soundVolume = soundVolume;
-        this.resolution = resolution;
+    // getInstance() 호출 시 매개변수를 아무것도 안 받기 때문에 기본 생성자
+    private GameConfigManager() {
+
     }
+
+//만약
+// private int soundVolume;           // 기본값 50
+// private String resolution; // 기본 해상도          이고 기본값이 50, 1280*720 일 땐
+// AllArguConstructor 로 생성자 만들어줘야함
+
+
+//    public GameConfigManager(int soundVolume, String resolution) {
+//        this.soundVolume = soundVolume;
+//        this.resolution = resolution;
+//    }
+//
+//    public static GameConfigManager getInstance() {
+//        if (Objects.isNull(instance)) {
+//            instance = new GameConfigManager(50, "1280*720");
+//        }
+//        return instance;
+//    }
 
     // TODO: 싱글톤 인스턴스 반환 메서드 구현
     public static GameConfigManager getInstance() {
         if (Objects.isNull(instance)) {
-            instance = new GameConfigManager(0, "");
+            instance = new GameConfigManager();
         }
         return instance;
     }
-    //null 인지 확인해서 null, 즉 최초의 한번만 생성하도록
-
 
     // Getter/Setter 메서드들
     public void setSoundVolume(int volume) {
@@ -46,6 +62,7 @@ public class GameConfigManager {
     }
 
     public String getConfigInfo() {
-        return String.format("Sound: %d, Resolution: %s", soundVolume, resolution);
+        return String.format("Sound: %d, Resolution: %s",
+                soundVolume, resolution);
     }
 }
