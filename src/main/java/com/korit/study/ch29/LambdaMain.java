f28b84fb088fb79424ccd56a2bab9bdd7bded533
jiny-01 <allie7019@naver.com>
2025-10-27 10:50:39 +0900

    ch29: Lambda ì

---
+++ src/main/java/com/korit/study/ch29/LambdaMain.java
@@ -0,0 +1,28 @@
+package com.korit.study.ch29;
+
+interface LambdaStudy {
+    int calc(int a, int b, int c);
+}
+
+public class LambdaMain {
+    public static void main(String[] args) {
+        // 익명 객체 생성 -
+        //1) 익명클래스 사용
+        LambdaStudy s1 = new LambdaStudy() {
+            @Override
+            public int calc(int a, int b, int c) {
+                System.out.println("익명클래스 사용");
+                return a * b - c;
+            }
+        };
+        System.out.println(s1.calc(10, 5, 2));
+
+        //2) 람다식 사용
+        LambdaStudy s2 = (a, b, c) -> {
+            System.out.println("람다 사용");
+            return a * b - c;
+        };
+        System.out.println(s2.calc(10, 5, 2));
+
+    }
+}
