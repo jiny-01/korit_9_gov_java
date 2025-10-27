8b3f4457fcda834937caacc16ba9865cc1a7b229
jiny-01 <allie7019@naver.com>
2025-10-27 10:38:13 +0900

    초2ch28: 익명클래스_버튼

---
+++ src/main/java/com/korit/study/ch28/ButtonMain.java
@@ -0,0 +1,46 @@
+package com.korit.study.ch28;
+interface ClickListener {
+    void onClick();
+}
+
+class Button {
+    private ClickListener listener;
+
+    public void setClickListener(ClickListener listener) {
+        this.listener = listener;
+    }
+
+    public void click() {
+        if (listener != null) {
+            listener.onClick();
+        }
+    }
+}
+
+public class ButtonMain {
+    public static void main(String[] args) {
+        Button button = new Button();
+
+        button.click();
+
+        // TODO: 여기에 익명 클래스를 사용하여 ClickListener 구현
+//        button.setClickListener(new ClickListener() {
+//            @Override
+//            public void onClick() {
+//                System.out.println("버튼이 클릭되었습니다.");
+//            }
+//        });
+
+        ClickListener clickListener = new ClickListener() {
+            @Override
+            public void onClick() {
+                System.out.println("버튼이 클릭되었습니다.");
+            }
+        };
+        button.setClickListener(clickListener);
+
+
+
+        button.click();
+    }
+}
